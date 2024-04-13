43c9de0ae7a85412bbf173adf702b1875591b85b
rayyanmoin12 <166857521+rayyanmoin12@users.noreply.github.com>
2024-04-13 14:31:33 +0500

    This is number guessing

---
+++ node_modules/@rayyanmoin12/cli-number-guessing/index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(randomNumber);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessNumber",
+        type: "Number",
+        message: "Please guess a number between 1 and 6:  ",
+    },
+]);
+if (answers.UserGuessNumber === randomNumber) {
+    console.log("Congratulations! You guessed correctly.");
+}
+else {
+    console.log("You guessed wrong number.");
+}
